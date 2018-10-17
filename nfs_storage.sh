b52457493ac636409ec8ccfbdcbddbe7c8f71a8e
bchangRep <34947323+bchangRep@users.noreply.github.com>
2018-10-17 10:33:16 -0400

    Create nfs_storage.sh

---
+++ nfs_storage.sh
@@ -0,0 +1,13 @@
+set -x
+
+sudo yum -y install nfs-utils
+
+sudo systemctl enable nfs-server.service
+sudo systemctl start nfs-server.service
+
+sudo mkdir /scratch
+sudo chmod 777 /scratch
+
+sudo mv /local/repository/exports /etc/exports
+
+sudo exportfs -a
