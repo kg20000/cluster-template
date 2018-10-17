3ffb96d4505b259994de1176b1551dc67f7e6360
bchangRep <34947323+bchangRep@users.noreply.github.com>
2018-10-17 12:51:08 -0400

    Update nfs_storage.sh

--- nfs_storage.sh
+++ nfs_storage.sh
@@ -1,12 +1,34 @@
+#!/bin/sh
 set -x
 
 sudo yum -y install nfs-utils
+
+
+yum install -y nfs-utils nfs-utils-lib
+chkconfig nfs on
+service rpcbind start
+service nfs start
+mkdir /software
+mkdir /scratch
+touch /scratch/machine_list
+touch /etc/exports
+
+for i in range (2,13):
+  echo "/software 192.168.1." + i + "(rw,sync,no_root_squash)" >> /etc/exports
+  echo "/scratch compute-" + i + "(rw,sync,no_root_squash)" >> /scratch/machine_list
+  
+	#echo "/software 192.168.1." + i + "(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+	#echo "/software compute-" + i + "(rw,sync,no_root_squash)" >> /etc/exports
+
+sudo mount -t nfs 192.168.1.2:/scratch /scratch
 
 sudo systemctl enable nfs-server.service
 sudo systemctl start nfs-server.service
 
 sudo mkdir /scratch
-sudo chmod 777 /scratch
+mpicc /scratch/hello.c -o /scratch/hello
+sudo chmod 755 /scratch
+
 
 sudo mv /local/repository/exports /etc/exports
 
