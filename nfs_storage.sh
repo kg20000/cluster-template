9b32efb00737320855514da00e79ee26ec43b490
Gavin Macko <36795443+Flamelawd@users.noreply.github.com>
2018-10-17 15:54:41 -0400

    Changing Storage process
    Information found online at: 
    https://www.tecmint.com/how-to-setup-nfs-server-in-linux/

--- nfs_storage.sh
+++ nfs_storage.sh
@@ -1,42 +1,27 @@
-#!/bin/sh
 
-sudo yum -y install nfs-utils nfs-utils-lib
-/etc/init.d/nfs start
+udo yum -y install nfs-utils nfs-utils-lib
+sudo yum -y install portmap
+
+/etc/init.d/portmap start
 /etc/init.d/nfs start
 chkconfig --level 35 portmap on
 chkconfig --level 35 nfs on
 
-chkconfig nfs on
-service rpcbind start
-service nfs start
-#mkdir /software
 sudo mkdir /scratch
-touch /scratch/machine_list
-touch /etc/exports
 
-for i in range (2,13):
-  echo "/software 192.168.1." + i + "(rw,sync,no_root_squash)" >> /etc/exports
-  echo "/scratch compute-" + i + "(rw,sync,no_root_squash)" >> /scratch/machine_list
-  
-	#echo "/software 192.168.1." + i + "(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-	#echo "/software compute-" + i + "(rw,sync,no_root_squash)" >> /etc/exports
+sudo vi /etc/exports
 
-sleep 180
-sudo mount -t nfs 192.168.1.1:/software /software
-sudo mount -t nfs 192.168.1.2:/scratch /scratch
-
-#sudo systemctl enable nfs-server.service
-#sudo systemctl start nfs-server.service
-
-cp /local/repository/source/* /scratch
-
-sudo mkdir /scratch
-mpicc /scratch/hello.c -o /scratch/hello
-sudo chmod 755 /scratch
-
-#mpicc /scratch/pi_mc.c -o /scratch/pi_mc
-#chmod 755 /scratch/pi_mc
-
-sudo mv /local/repository/exports /etc/exports
-
-sudo exportfs -a
+#TODO make to for loop 
+/software 192.168.1.3(rw,sync,no_root_squash)
+/software 192.168.1.4(rw,sync,no_root_squash)
+/software 192.168.1.5(rw,sync,no_root_squash)
+/software 192.168.1.6(rw,sync,no_root_squash)
+/software 192.168.1.7(rw,sync,no_root_squash)
+/software 192.168.1.8(rw,sync,no_root_squash)
+/software 192.168.1.9(rw,sync,no_root_squash)
+/software 192.168.1.10(rw,sync,no_root_squash)
+/software 192.168.1.11(rw,sync,no_root_squash)
+/software 192.168.1.12(rw,sync,no_root_squash)
+/software 192.168.1.13(rw,sync,no_root_squash)
+/software 192.168.1.14(rw,sync,no_root_squash)
+/software 192.168.1.15(rw,sync,no_root_squash)
