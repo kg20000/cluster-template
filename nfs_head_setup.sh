3f7a15cc388df67586c27918aed3ee4a50347556
bchangRep <34947323+bchangRep@users.noreply.github.com>
2018-10-17 12:51:10 -0400

    Update nfs_head_setup.sh

--- nfs_head_setup.sh
+++ nfs_head_setup.sh
@@ -11,10 +11,13 @@ for i in range (2,13):
 	#echo "/software 192.168.1." + i + "(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 	#echo "/software compute-" + i + "(rw,sync,no_root_squash)" >> /etc/exports
 
+#sudo service rpcbind start
 #sudo service nfs start
 #sudo service portmap start
 #sudo mkdir /mnt/scratch
 #sudo mount -t nfs storage:/scratch /mnt/scratch
+mkdir /software
+mount -t nfs 192.168.1.1:/software /software
 
 set -x
 sudo yum -y group install "Development Tools"
