a65c9466c14eafd8e629337673d66f1f66027bfd
bchangRep <34947323+bchangRep@users.noreply.github.com>
2018-10-17 14:09:07 -0400

    Update nfs_head_setup.sh

--- nfs_head_setup.sh
+++ nfs_head_setup.sh
@@ -20,22 +20,3 @@ for i in range (2,13):
 #mount -t nfs 192.168.1.1:/software /software
 
 #exportfs -a
-
-set -x
-sudo yum -y group install "Development Tools"
-cd /software
-sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
-sudo tar xzf openmpi-3.1.2.tar.gz
-cd openmpi-3.1.2
-sudo ./configure --prefix=/software
-sudo make
-sudo make all install
-#echo "export PATH='$PATH:/opt/openmpi/3.1.2/bin'" >> /users/BC843101/.bashrc
-#echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/openmpi/3.1.2/lib/'" >> /users/BC843101/.bashrc
-#Modified PATHS
-echo "export PATH='$PATH:/software/bin'" >> /users/BC843101/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib'" >> /users/BC843101/.bashrc
-cd ..
-mkdir /software/testFolder
-sudo rm -Rf openmpi-3.1.2
-sudo rm -Rf openmpi-3.1.2.tar.gz
