2b273d665031426b2c3d787aabd5a7aa0db1cf01
bchangRep <34947323+bchangRep@users.noreply.github.com>
2018-10-18 00:31:27 -0400

    Update mountStorage.sh

--- mountStorage.sh
+++ mountStorage.sh
@@ -3,5 +3,5 @@ set -x
 sudo yum -y install nfs-utils-lib
 sudo mount -t nfs 192.168.1.3:/scratch /mnt/scratch
 
-vi /etc/fsab
-192.168.1.3:/scratch /mnt nfs default 0 0
+echo '
+192.168.1.3:/scratch /mnt  nfs defaults 0 0' >> /etc/fstab
