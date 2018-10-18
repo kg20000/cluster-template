60c5d8c4b55f48e5506e860baa3b7be31a026e62
bchangRep <34947323+bchangRep@users.noreply.github.com>
2018-10-18 00:36:24 -0400

    Update nfs_storage_setup.sh

--- nfs_storage_setup.sh
+++ nfs_storage_setup.sh
@@ -13,7 +13,7 @@ sudo systemctl start nfs-server.service
 
 sudo vi /etc/exports
 
-#TODO make to for loop 
+echo '
 /software 192.168.1.3(rw,sync,no_root_squash)
 /software 192.168.1.4(rw,sync,no_root_squash)
 /software 192.168.1.5(rw,sync,no_root_squash)
@@ -26,4 +26,4 @@ sudo vi /etc/exports
 /software 192.168.1.12(rw,sync,no_root_squash)
 /software 192.168.1.13(rw,sync,no_root_squash)
 /software 192.168.1.14(rw,sync,no_root_squash)
-/software 192.168.1.15(rw,sync,no_root_squash)
+/software 192.168.1.15(rw,sync,no_root_squash)' >> /etc/exports
