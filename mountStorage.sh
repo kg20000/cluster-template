c31609cec5e4f573cc0f5778cacd4b387c3dd8b7
bchangRep <34947323+bchangRep@users.noreply.github.com>
2018-10-18 12:52:43 -0400

    prettier

--- mountStorage.sh
+++ mountStorage.sh
@@ -1,16 +1,14 @@
 set -x
 sudo yum -y install nfs-utils nfs-utils-lib
 
-#sudo mkdir -p /users/BC843101/software
+sudo mkdir -p /users/BC843101/software
 sudo mkdir -p /users/BC843101/scratch
 
-#sudo mount -t nfs 192.168.1.1:/software /users/BC843101/software
+sudo mount -t nfs 192.168.1.1:/software /users/BC843101/software
 sudo mount -t nfs 192.168.1.3:/scratch /users/BC843101/scratch
 
-#echo '
-#192.168.1.1:/software /users/BC843101/software  nfs defaults 0 0' >> /etc/fstab
-echo '
-192.168.1.3:/scratch /users/BC843101/scratch  nfs defaults 0 0' >> /etc/fstab
+echo '192.168.1.1:/software /users/BC843101/software  nfs defaults 0 0' >> /etc/fstab
+echo '192.168.1.3:/scratch /users/BC843101/scratch  nfs defaults 0 0' >> /etc/fstab
 
 echo "export PATH='$PATH:/users/BC843101/software/openmpi/3.1.2/bin'" >> /users/BC843101/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/users/BC843101/software/openmpi/3.1.2/lib/'" >> /users/BC843101/.bashrc
