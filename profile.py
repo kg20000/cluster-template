00b730d0512c3c1bc37b34dc372a35cbdf29ab5c
bchangRep <34947323+bchangRep@users.noreply.github.com>
2018-10-18 14:17:05 -0400

    updated

--- profile.py
+++ profile.py
@@ -83,14 +83,12 @@ for i in range(0, 4):
 		node = request.XenVM("compute-" + str(i-2))
 		node.cores = 4
 		node.ram = 4096
-		#node.addService(pg.Execute(shell="sh", command="sudo mkdir /software"))
-		#node.addService(pg.Execute(shell="sh", command="sudo mkdir /scratch"))
-		#node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.1:/software /software"))
 		
 		# addServices to call bash scripts to add local mount points to client nodes for NFS's
 		node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/mountHead.sh"))
 		node.addService(pg.Execute(shell="sh", command="sudo /local/repository/mountHead.sh"))
-		
+		node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/mountStorage.sh"))
+		node.addService(pg.Execute(shell="sh", command="sudo /local/repository/mountStorage.sh"))		
 		# copy files to scratch
 		node.addService(pg.Execute(shell="sh", command="sudo cp /local/repository/source/* /scratch"))
 		node.addService(pg.Execute(shell="sh", command="sudo cp /local/repository/source/* /users/BC843101/scratch"))
