dbe5a17615fd07ee10988e72d3ae5176098e0b4a
bchangRep <34947323+bchangRep@users.noreply.github.com>
2018-10-18 00:30:16 -0400

    Update mountHead.sh

--- mountHead.sh
+++ mountHead.sh
@@ -1,6 +1,16 @@
 set -x
 sudo yum -y install nfs-utils nfs-utils-lib
-sudo mount -t nfs 192.168.1.1:/software /mnt/software
 
-vi /etc/fstab
-192.168.1.1:/software /mnt nfs defaults 0 0
+sudo mkdir -p /users/BC843101/software
+sudo mkdir -p /users/BC843101/scratch
+
+sudo mount -t nfs 192.168.1.1:/software /users/BC843101/software
+sudo mount -t nfs 192.168.1.3:/scratch /users/BC843101/scratch
+
+echo '
+192.168.1.1:/software /users/al844976/software  nfs defaults 0 0' >> /etc/fstab
+echo '
+192.168.1.3:/scratch /users/al844976/scratch  nfs defaults 0 0' >> /etc/fstab
+
+echo "export PATH='$PATH:/users/BC843101/software/openmpi/3.1.2/bin'" >> /users/BC843101/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/users/BC843101/software/openmpi/3.1.2/lib/'" >> /users/BC843101/.bashrc
